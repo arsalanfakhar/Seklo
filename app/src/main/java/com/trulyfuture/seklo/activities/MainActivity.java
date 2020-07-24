1f6f85fcf9f350cd69a708384752d1e622d35a9f
Arsalan Fakhar Siddiqui <arsalan.fakhar@yahoo.com>
2020-07-24 18:56:46 +0500

    Added bottom nav bar and login signup UI

--- app/src/main/java/com/trulyfuture/seklo/activities/MainActivity.java
+++ app/src/main/java/com/trulyfuture/seklo/activities/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.trulyfuture.seklo.R;
+import com.trulyfuture.seklo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setItemIconTintList(null);
     }
 }
